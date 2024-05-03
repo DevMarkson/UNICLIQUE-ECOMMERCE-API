5d5ad7cfcb90fbd1a252c4ff826749adb033b0eb
Markson17 <marksonfrosh@gmail.com>
2024-05-03 08:27:04 +0100

    Update app.js and connect.js files

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const path = require('path');
+const path = require("path");
 const dotenv = require("dotenv");
 const express = require("express");
+const connectDB = require("./db/connect");
+const productsRouter = require('./routers/productRoute')
+require('express-async-errors');
 const app = express();
 
 // load config
 dotenv.config({
-    path: './config/config.env'
-})
+  path: "./config/config.env",
+});
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -19,6 +22,8 @@ app.get("/", (req, res) => {
   res.send('<h1>Product API</h1><a href="/api/products">products route</a>');
 });
 
+app.use('/api/v1/products', productsRouter)
+
 // products route
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -28,6 +33,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // connectDB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening at port ${port}...`));
   } catch (error) {
     console.log(error);
--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
