b5e5e525dbe59037e66dc93f7b52277e0999bd80
Markson17 <marksonfrosh@gmail.com>
2024-05-06 17:28:22 +0100

    added the OrderModel.js

---
+++ models/OrderModel.js
@@ -0,0 +1,53 @@
+const { required } = require('joi');
+const mongoose = require('mongoose');
+
+const SingleOrderItemSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: String,
+        required: true
+    },
+    product:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+});
+
+const OrderSchema = mongoose.Schema({
+    shippingFee:{
+        type: Number,
+        required: true,
+    },
+    subtotal:{
+        type: Number,
+        required: true,
+    },
+    total:{
+        type: Number,
+        required: true,
+    },
+    orderItems: [SingleOrderItemSchema],
+    status:{
+        type: String,
+        enum: ['pending', 'paid', 'delivered', 'failed', 'canceled'],
+        default: 'pending',
+    },
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model('Order', OrderSchema);
