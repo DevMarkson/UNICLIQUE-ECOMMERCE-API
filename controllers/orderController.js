81e9427cca4312903cc0736c64bd5aacf70352b9
Markson17 <marksonfrosh@gmail.com>
2024-05-07 22:13:46 +0100

    Refactor orderController.js and createOrder function

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -12,4 +12,51 @@ const StripeAPI = async ({ amount, currency }) =>{
 
 const createOrder = async (req, res) =>{
     const { items: cartItems, charges, ShippingFee} = req.body
+
+    if (!cartItems || cartItems.length < 1){
+        throw new CustomError.BadRequestError('No cart items provided');
+    }
+
+    if (!charges || !ShippingFee){
+        throw new CustomError.BadRequestError(
+            'Please provide charges and shipping fee'
+        );
+    }
+
+    let orderItems = [];
+    let subtotal = 0;
+
+    for (const item of cartItems){
+        const dbProduct = await Product.findOne({ _id: item.product});
+        if (!dbProduct){
+            throw new CustomError.NotFoundError(
+                `No product with id : ${item.product}`
+            );
+        }
+        const {name, price, image, _id} = dbProduct
+        const SingleOrderItem = {
+            amount: item.amount,
+            name,
+            price,
+            image,
+            product: _id,
+        };
+
+        // add items to order
+        orderItems = [...orderItems, SingleOrderItem];
+        // calculate suntotal
+        subtotal += item.amount * price;
+    }
+    // calculate total
+    const total = charges + ShippingFee + subtotal;
+
+    const order = await Order.create({
+        orderItems,
+        total,
+        subtotal,
+        charges,
+        ShippingFee
+    });
+
+    res.status(StatusCodes.CREATED).json({ order})
 }
