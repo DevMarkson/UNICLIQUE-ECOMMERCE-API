6a56707378911c4ce4a2259d6c3893c5ba842fd5
Markson17 <marksonfrosh@gmail.com>
2024-05-09 00:29:19 +0100

    Add validation and constraints to User model

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,48 +1,56 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: true,
+    required: [true, 'Please provide a first name'],
+    minlength: 3,
+    maxlength: 50,
   },
   lastName: {
     type: String,
-    required: true,
+    required: [true, 'Please provide a last name'],
+    minlength: 3,
+    maxlength: 50,
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please provide email'],
     unique: true,
+    validate:{
+        validator: validator.isEmail,
+        message: 'Please provide valid email'
+    },
   },
   phoneNumber: {
     type: String,
-    required: true,
+    required: [true, 'Please prove a number'],
   },
   password: {
     type: String,
-    required: true,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
   },
 });
 
 // Hash the password before saving
 UserSchema.pre('save', async function (next) {
-  try {
+    if (!this.isModified('password')) return;
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
 });
 
 // Compare the provided password with the hashed password
 UserSchema.methods.comparePassword = async function (candidatePassword) {
-  try {
-    return await bcrypt.compare(candidatePassword, this.password);
-  } catch (err) {
-    throw err;
-  }
+    const isMatch =  await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
 };
 
 module.exports = mongoose.model('User', UserSchema);
