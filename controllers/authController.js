d0517c074fc15e923c356cc2ae2052d31e3a073e
Markson17 <marksonfrosh@gmail.com>
2024-05-10 03:03:20 +0100

    refactor: Separate user registration logic into a dedicated function

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,23 +3,22 @@ const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const { attachCookiesToResponse, createTokenUser } = require("../utils");
-const { validateEmail, validatePassword } = require("../utils/userValidators");
 
 // Register a new user
 const register = async (req, res) => {
   const { firstName, lastName, email, phoneNumber, password } = req.body;
 
-//   // Validate input
-//   const isValidEmail = validateEmail(email);
-//   const isValidPassword = validatePassword(password);
+  // Validate input
+  const isValidEmail = validateEmail(email);
+  const isValidPassword = validatePassword(password);
 
-//   if (!isValidEmail || !isValidPassword) {
-//     throw new CustomError.BadRequestError("Invalid input");
-//   }
+  if (!isValidEmail || !isValidPassword) {
+    throw new CustomError.BadRequestError("Invalid input");
+  }
 
-//   if (password !== confirmPassword) {
-//     throw new CustomError.BadRequestError("Passwords do not match");
-//   }
+  if (password !== confirmPassword) {
+    throw new CustomError.BadRequestError("Passwords do not match");
+  }
 
   // Check if the email already exists
   const emailAlreadyExists = await User.findOne({ email });
