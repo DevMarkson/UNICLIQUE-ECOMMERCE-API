640cc2d91b9c825598c7c250c500bb39702fd0a5
Markson17 <marksonfrosh@gmail.com>
2024-05-10 01:20:18 +0100

    refactor: Separate user registration logic into a dedicated function

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,92 +1,45 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const {StatusCodes} = require('http-status-codes');
 const CustomError = require('../errors');
+const {attachCookiesToResponse, createTokenUser} = require('../utils');
 const { validateEmail, validatePassword } = require('../utils/userValidators');
-// const { errorHandler } = require('../utils/errorHandler');
-// const redisClient = require('../utils/redis');
 
-// Authenticate a user and generate a JWT
-exports.authenticateUser = async (req, res, next) => {
-  const { email, password } = req.body;
-
-  try {
-    // Validate input
-    const isValidEmail = validateEmail(email);
-    const isValidPassword = validatePassword(password);
-
-    if (!isValidEmail || !isValidPassword) {
-      return res.status(400).json({ msg: 'Invalid email or password' });
-    }
-
-    // Check if the user exists
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ msg: 'Invalid credentials' });
-    }
-
-    // Verify the password
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) {
-      return res.status(401).json({ msg: 'Invalid credentials' });
-    }
-
-    // Generate a JWT token
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    // Cachingin Redis
-    // await redisClient.setex(`auth_${user.id}`, 3600, token);
-    
-     // Respond with the welcome message
-     res.json({ msg: `Welcome back, ${user.firstName}`, token });
-  } catch (err) {
-    next(err);
-  }
-};
 
 // Register a new user
-exports.registerUser = async (req, res, next) => {
-  const { firstName, lastName, email, phoneNumber, password, confirmPassword } = req.body;
+const register = async (req, res) => {
+    const { firstName, lastName, email, phoneNumber, password, confirmPassword } = req.body;
 
-  try {
     // Validate input
     const isValidEmail = validateEmail(email);
     const isValidPassword = validatePassword(password);
     const isValidConfirmPassword = validatePassword(confirmPassword);
 
     if (!isValidEmail || !isValidPassword || !isValidConfirmPassword) {
-      return res.status(400).json({ msg: 'Invalid input' });
+        throw new CustomError.BadRequestError('Invalid input' );
     }
 
     if (password !== confirmPassword) {
-      return res.status(400).json({ msg: 'Passwords do not match' });
+        throw new CustomError.BadRequestError('Passwords do not match' );
     }
 
-    // Check if the user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ msg: 'User already exists' });
+    // Check if the email already exists
+    const emailAlreadyExists = await User.findOne({ email });
+    if (emailAlreadyExists) {
+        throw new CustomError.BadRequestError('Email already exists' );
     }
 
-    // Create a new user
-    const newUser = new User({
-      firstName,
-      lastName,
-      email,
-      phoneNumber,
-      password,
-    });
+    // first registered user is an admin
+    const isFirstAccount = (await User.countDocuments({})) === 0;
+    const role = isFirstAccount ? 'admin' : 'user';
 
-    // Save the new user
-    const savedUser = await newUser.save();
-
-    res.status(201).json(savedUser);
-  } catch (err) {
-    next(err);
-  }
+    const user = await User.create({firstName, lastName, email, phoneNumber, password, role});
+    const tokenUser = createTokenUser(user);
+    attachCookiesToResponse({res, user: tokenUser});
+    res.status(StatusCodes.CREATED).json({ user: tokenUser});
 };
+
+
+module.exports = {
+    register,
+}
